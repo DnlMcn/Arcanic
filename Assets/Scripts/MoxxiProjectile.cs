8806a4ee82f1b7bacc80794deec209a7aed2b6bc
deogos <diogo.alonso@etec.sp.gov.br>
2022-08-21 15:38:59 -0300

    OnCollisionEnter e outras alterações
    - Adicionado OnCollisionEnter no script Moxxi Projectile. (Não funciona)
    - Adição do EnemyManager gameobject perdido por causa do merge.

--- Assets/Scripts/MoxxiProjectile.cs
+++ Assets/Scripts/MoxxiProjectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoxxiProjectile : MonoBehaviour
 {
-    
+
     [SerializeField] private float speed = 70f;
     private Vector3 velocity;
 
@@ -12,5 +12,14 @@ public class MoxxiProjectile : MonoBehaviour
     {
         velocity = Vector3.forward * speed * Time.deltaTime;
         transform.Translate(velocity);
+
+    }
+    private void OnColissionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
+
