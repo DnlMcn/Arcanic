315dc992ec5c2f62c4d06b5fe7bbbfeedf431b11
Danilo Mancini <haomakk.himself@gmail.com>
2022-09-30 09:44:31 -0300

    Add in-script explanation of auto-firing function

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,6 +5,15 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(PlayerInput))]
 
+
+//  Explanation of the "Automatic" function of this script:
+//
+//  Whenever the primary attack input is performed, the "Fire" method checks if "isAuto" is set to true. 
+//  If so, the variable "isShooting" is set to true. 
+//  If the player cancels the shooting action (by releasing LMB), "isShooting" is set to false.
+//  If "isShooting" is set to true, the "Fire" method tries to be called in the "Update" method.
+
+
 public class Shoot : MonoBehaviour
 {
     [SerializeField] PlayerCharacter character;
@@ -40,7 +49,7 @@ public class Shoot : MonoBehaviour
 
     private void Update()
     {
-        if (isShooting) Fire();
+        if (isShooting && canShoot) Fire();
     }
 
     private void OnEnable()
@@ -57,13 +66,13 @@ public class Shoot : MonoBehaviour
     {
         if (canShoot)
         {
+            if (isAuto) isShooting = true;
             // Read the spawn point of the projectile as the position of the child GameObject
             Vector3 projectileSpawnPoint = transform.Find("ShootingPoint").position;
             // Instantiate the projectile at the spawn point with the player's rotation
             Instantiate(character.primary.projectile.prefab, projectileSpawnPoint, transform.rotation);
             canShoot = false;
             StartCoroutine(ShootingCooldown());
-            if (isAuto) isShooting = true;
         }
     }
 
