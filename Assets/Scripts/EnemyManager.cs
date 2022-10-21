98824dd39a2f0ded037c309e59162a3b16389d22
DnlMcn <haomakk.himself@gmail.com>
2022-10-21 10:43:37 -0300

    Update EnemyManager.cs

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -9,7 +9,6 @@ public class EnemyManager : MonoBehaviour
     public Transform[] SpawnPoints;
     public GameObject EnemyPrefab;
 
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(EnemySpawning());
@@ -29,8 +28,5 @@ public class EnemyManager : MonoBehaviour
         int randomNumber = Mathf.RoundToInt(Random.Range(0f, SpawnPoints.Length-1));
 
         Instantiate(EnemyPrefab, SpawnPoints[randomNumber].transform.position, Quaternion.identity);
-
-
     }
-
 }
