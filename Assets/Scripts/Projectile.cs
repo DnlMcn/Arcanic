2ea8873a8a02f5b0b96ef1d4856f68a31d045a2c
DnlMcn <haomakk.himself@gmail.com>
2022-10-21 08:41:29 -0300

    Raycast collision for bullets
    - Bullets will no longer go through enemies without colliding with them;
    - Add a bool for projectile ScriptableObjects to define if they can pierce enemies;

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,7 +8,11 @@ public class Projectile : MonoBehaviour
     private float speed;
     private float lifespan;
     private float damage;
+
     private Vector3 velocity;
+    private Vector3 lastFramePosition;
+
+    [SerializeField] LayerMask mask;
 
     void Start()
     {
@@ -18,9 +22,29 @@ public class Projectile : MonoBehaviour
         StartCoroutine(ProjectileLifespan());
     }
 
-    void Update()
+    private void FixedUpdate() 
     {
+        lastFramePosition = transform.position;
         Move();
+        DetectRayCollision();
+    }
+
+    private void DetectRayCollision()
+    {
+        Ray ray = new Ray(lastFramePosition, transform.forward);
+        RaycastHit hitInfo;
+
+        float lastToCurrentFrameDistace = Vector3.Distance(lastFramePosition, transform.position);
+        if (Physics.Raycast(ray, out hitInfo, lastToCurrentFrameDistace, mask))
+        {
+            if (hitInfo.collider.gameObject.TryGetComponent<BasicEnemy>(out BasicEnemy enemyComponent))
+            {
+                if (!projectileType.piercing) Destroy(transform.gameObject);
+                enemyComponent.TakeDamage(damage);
+            }
+        }
+
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * lastToCurrentFrameDistace, Color.red);
     }
 
     void Move()
@@ -29,15 +53,6 @@ public class Projectile : MonoBehaviour
         transform.Translate(velocity);
     }
     
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.TryGetComponent<BasicEnemy>(out BasicEnemy enemyComponent))
-        {
-            Destroy(transform.gameObject);
-            enemyComponent.TakeDamage(damage);
-        }
-    }
-
     IEnumerator ProjectileLifespan()
     {
         yield return new WaitForSeconds(lifespan);
