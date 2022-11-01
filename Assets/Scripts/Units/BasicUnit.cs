e4eefef66d0d7901d468f77d44c4658d161a88a5
DnlMcn <haomakk.himself@gmail.com>
2022-11-01 08:26:16 -0300

    h

--- Assets/Scripts/Units/BasicUnit.cs
+++ Assets/Scripts/Units/BasicUnit.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class BasicUnit : MonoBehaviour
 {
-    EnemyRuntimeSet globalEnemyRuntimeSet;
+    [SerializeField] EnemyRuntimeSet globalEnemyRuntimeSet;
 
-
-
-    [SerializeField] protected TroopSO type;
-    protected TroopLevelSO level;
+    [SerializeField] protected UnitSO type;
+    protected UnitLevelSO level;
     protected int levelIndex = 0;  // Índice usado para selecionar o nível da tropa
     protected int pole = 0;
 
@@ -23,9 +21,7 @@ public class BasicUnit : MonoBehaviour
     
     void Start()
     {
-        level = type.GetTroopLevel(levelIndex);
-
-        globalEnemyRuntimeSet = UnitManager.GlobalEnemyRuntimeSet;
+        level = type.GetUnitLevel(levelIndex);
     }
 
     void Update()
@@ -35,7 +31,28 @@ public class BasicUnit : MonoBehaviour
 
     void MoveTowardsClosestEnemy()
     {
-        transform.LookAt(SpatialCalc.GetClosestEnemy(globalEnemyRuntimeSet, transform));
+        Transform target = GetClosestEnemy(globalEnemyRuntimeSet, transform);
+        transform.LookAt(target);
         transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+    }
+
+    public Transform GetClosestEnemy(RuntimeSet<BasicEnemy> potentialTargets, Transform fromThis)
+    {
+        Transform bestTarget = null;
+        float closestDistance = Mathf.Infinity;
+        Vector3 currentPosition = fromThis.position;
+
+        for(int i = 0; i < potentialTargets.Items.Count; i++)
+        {
+            Transform potentialTarget = potentialTargets.Items[i].transform;
+            float distanceToTarget = Vector3.Distance(fromThis.position, potentialTarget.position);
+            if(distanceToTarget < closestDistance)
+            {
+                closestDistance = distanceToTarget;
+                bestTarget = potentialTarget;
+            }
+        }  
+
+        return bestTarget;
     }
 }
