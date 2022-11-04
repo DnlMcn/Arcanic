7696fde539a7ca5832386507a1eb8fcac5b07eb0
deogos <78805025+deogos@users.noreply.github.com>
2022-11-04 08:35:41 -0300

    Update Main Menu
    - All buttons works expect options
    - TODO : Options and saves

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI
-using UnityEngine.SceneManagement
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Levels To Load:")]
 
+    public string _newGameLevel;
+    private string levelToLoad;
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if(PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+
+    public void ExitButton(){
+        Application.Quit();
+    }
 }
