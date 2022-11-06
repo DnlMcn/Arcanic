eee7f8d18b271203ee486203540b3ec46bed570c
deogos <diogo.alonso@etec.sp.gov.br>
2022-11-06 20:47:15 -0300

    Changes on menu
    -Added a lot of things in the menu
    - Dont terminado yet

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Setting:")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [Header("Confirmation:")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Levels To Load:")]
 
     public string _newGameLevel;
@@ -32,5 +40,26 @@ public class MenuController : MonoBehaviour
 
     public void ExitButton(){
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
